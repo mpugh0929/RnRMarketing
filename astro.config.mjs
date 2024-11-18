82278d08dd47a67bf1c8d67adc4b25714b34f29c
Matt Pugh <mpugh0929@gmail.com>
2024-11-17 22:23:08 -0500

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import icon from "astro-icon";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()]
+  integrations: [icon(), tailwind(), sitemap()]
 });
